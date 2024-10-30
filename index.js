e06647f10181fbd9169d8d24d14ebcc828d7f2c1
JCTSC <jctscamargo@gmail.com>
2024-10-30 13:45:14 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const prisma = new PrismaClient();
+
+const express = require("express");
+
+const app = express();
+app.use(express.json())
+
+const authRouters = require("./routes/authRoutes");
+app.use("/auth", authRouters);
+
+app.listen(8000);
---
+++ testaPrisma.js
@@ -0,0 +1,26 @@
+
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+async function main() {
+    // Insire um usuário
+    const usuario = await prisma.usuario.create({
+        data: {
+            nome: "Maria",
+            email: "maria@example.com",
+            password: "senha123",
+            tipo: "cliente",
+        },
+    });
+
+    console.log("Novo Usuário: " + JSON.stringify(usuario));
+
+    // Busca usuários
+    const usuarios = await prisma.usuario.findMany();
+    console.log("Todos os usuários: " + JSON.stringify(usuarios));
+    console.log(usuario);
+    
+
+}
+
+main();
