5ae669e4622f4eb86f3cd300391876b5327b2450
Bruninhu <brunoacristo13@gmail.com>
2025-01-25 00:34:26 -0300

    arrumar algumas coisas depois

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const prisma = require('./prisma/prismaClient')
 
-const cors = require("cors")
-
 const express= require('express')
 const app = express()
 app.use(express.json())
-app.use(cors())
+
+const cors = require('cors');
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 const authRoutes = require('./routes/authRoutes')
-
 app.use('/auth', authRoutes)
+
+
+const AuthController = require('../controllers/AuthControllers');
+const tableRoutes = require('./routes/tableRoutes')   
+app.use('/table', tableRoutes)
+
+
+const admRoutes = require('./routes/admRoutes');
+app.use('/admin',  admRoutes);
 
 app.listen(8000)
 
